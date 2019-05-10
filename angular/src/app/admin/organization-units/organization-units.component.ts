03712648624b5d983842373585504e1cf70b775f
Vũ Quyền <wakawaka2610@gmail.com>
2019-05-11 06:41:48 +0700

    Place product to organization unit (demo).

--- angular/src/app/admin/organization-units/organization-units.component.ts
+++ angular/src/app/admin/organization-units/organization-units.component.ts
@@ -3,6 +3,8 @@ import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { OrganizationTreeComponent } from './organization-tree.component';
 import { OrganizationUnitMembersComponent } from './organization-unit-members.component';
+import { OrganizationUnitServiceProxy, ProductsToOrganizationUnitInput } from '@shared/service-proxies/service-proxies';
+import { NotifyService } from 'abp-ng2-module/dist/src/notify/notify.service';
 
 @Component({
     templateUrl: './organization-units.component.html',
@@ -12,10 +14,26 @@ export class OrganizationUnitsComponent extends AppComponentBase {
 
     @ViewChild('ouMembers') ouMembers: OrganizationUnitMembersComponent;
     @ViewChild('ouTree') ouTree: OrganizationTreeComponent;
-
-    constructor(
-        injector: Injector
+    productIds: string = '11,12';
+    organizationUnitId: number = 9;
+    constructor(injector: Injector,
+        private organizationUnitService: OrganizationUnitServiceProxy,
     ) {
         super(injector);
+    }
+
+    placeProductsToOrganizationUnit() {
+        let productIds_listString = this.productIds.split(',');
+        let productIds_listNumber = productIds_listString.map(productId => {
+            return Number(productId);
+        });
+        let input = new ProductsToOrganizationUnitInput(
+            {
+                productIds: productIds_listNumber,
+                organizationUnitId: this.organizationUnitId
+            });
+        this.organizationUnitService.placeProductToOrganizationUnit(input).subscribe(result => { 
+            this.notify.info(this.l('PlaceSuccessfully'));
+        });
     }
 }
