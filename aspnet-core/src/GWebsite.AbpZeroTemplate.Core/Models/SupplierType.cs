188c8b69f399841c3004662fca4d7ee40c46cca5
hoideptrai123 <toilahoi007@gmail.com>
2019-05-31 11:04:20 +0700

    fix stype

--- aspnet-core/src/GWebsite.AbpZeroTemplate.Core/Models/SupplierType.cs
+++ aspnet-core/src/GWebsite.AbpZeroTemplate.Core/Models/SupplierType.cs
@@ -8,7 +8,6 @@ namespace GWebsite.AbpZeroTemplate.Core.Models
     {
         public string Code { get; set; }
         public string Name { get; set; }
-<<<<<<< HEAD
         public string Code { get; set; }
         public string Note { get; set; }
         public int Status { get; set; }
@@ -17,10 +16,5 @@ namespace GWebsite.AbpZeroTemplate.Core.Models
         {
             Suppliers = new Collection<Supplier>();
         }
-=======
-        public string Note { get; set; }
-        public int Status { get; set; }
-        public bool IsInCludeSupplier { get; set; }
->>>>>>> 1556a7986e8ce87e42bdd0c2c51c4419e5bcaab6
     }
 }
