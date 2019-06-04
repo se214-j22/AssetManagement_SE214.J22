2df97377b7cc39dfec4ffaed1ea5a0fb31154b07
hoideptrai123 <toilahoi007@gmail.com>
2019-06-04 09:30:21 +0700

    add bid profile

---
+++ aspnet-core/src/GWebsite.AbpZeroTemplate.Core/BidProfile.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GWebsite.AbpZeroTemplate.Core
+{
+  public partial  class BidProfile :Entity<int>
+    {
+        public int Index { get; set; }
+        //public string Code { get; set; }
+        //public string Code { get; set; }
+        //public string Code { get; set; }
+        //public string Code { get; set; }
+        //public string Code { get; set; }
+        //public string Code { get; set; }
+        //public string Code { get; set; }
+
+
+    }
+}
