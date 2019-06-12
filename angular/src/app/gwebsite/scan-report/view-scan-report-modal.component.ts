34973a95a98a983d49b768264e0c40c12c6bd6df
Tran Dinh Khang <khangtd.09@gmail.com>
2019-06-12 13:14:36 +0700

    git push

--- angular/src/app/gwebsite/scan-report/view-scan-report-modal.component.ts
+++ angular/src/app/gwebsite/scan-report/view-scan-report-modal.component.ts
@@ -1,9 +1,9 @@
-import { ScanReportForViewDto } from './../../../shared/service-proxies/service-proxies';
-import { AppComponentBase } from "@shared/common/app-component-base";
-import { AfterViewInit, Injector, Component, ViewChild } from "@angular/core";
-import { ScanReportServiceProxy } from "@shared/service-proxies/service-proxies";
-import { Router } from "@angular/router";
-import { ModalDirective } from 'ngx-bootstrap';
+import {ScanReportForViewDto} from './../../../shared/service-proxies/service-proxies';
+import {AppComponentBase} from '@shared/common/app-component-base';
+import {AfterViewInit, Injector, Component, ViewChild} from '@angular/core';
+import {ScanReportServiceProxy} from '@shared/service-proxies/service-proxies';
+import {Router} from '@angular/router';
+import {ModalDirective} from 'ngx-bootstrap';
 
 @Component({
     selector: 'viewScanReportModal',
@@ -13,7 +13,7 @@ import { ModalDirective } from 'ngx-bootstrap';
 export class ViewScanReportModalComponent extends AppComponentBase {
     objectKeys = Object.keys;
     scanReportData: object;
-    scanReport : ScanReportForViewDto;
+    scanReport: ScanReportForViewDto;
     @ViewChild('viewModal') modal: ModalDirective;
 
     constructor(
@@ -22,6 +22,7 @@ export class ViewScanReportModalComponent extends AppComponentBase {
     ) {
         super(injector);
     }
+
     close() {
         this.modal.hide();
     }
@@ -32,17 +33,62 @@ export class ViewScanReportModalComponent extends AppComponentBase {
             this.scanReport = result;
             this.scanReportData = JSON.parse(this.scanReport.scannedData);
             this.modal.show();
-        })
+        });
 
     }
+
     computeLabel(label: string): string {
         switch (label) {
-            case "cpuManufacture":
-                return "Nhà sản xuất CPU";
-            case "cpuCores":
-                return "Xung nhịp CPU";
-            case "biosMaker":
-                return "Nhà sản xuất Bios";;
+            case 'cpuManufacture':
+                return 'Nhà sản xuất CPU';
+            case 'cpuCores':
+                return 'Xung nhịp CPU';
+            case 'biosMaker':
+                return 'Nhà sản xuất Bios';
+            case 'boardMaker':
+                return 'Nhà sản xuất bảng mạch chủ';
+            case 'ramSlots':
+                return 'Số lượng khe RAM';
+            case 'memory':
+                return 'Bộ nhớ RAM';
+            case 'computerName':
+                return 'Tên máy tính';
+            case 'osInformation':
+                return 'Thông tin hệ điều hành';
+            case 'macAddress':
+                return 'Địa chỉ MAC';
+            case 'accountName':
+                return 'Tên tài khoản';
+            case 'driveName':
+                return 'Tên ổ cứng';
+            case 'driveType':
+                return 'Loại ổ cứng';
+            case 'volumeLabel':
+                return 'Phân vùng';
+            case 'driveFormat':
+                return 'Loại ổ cứng';
+            case 'availableFreeSpace':
+                return 'Bộ nhớ khả dụng';
+            case 'totalFreeSpace':
+                return 'Tổng bộ nhớ trống';
+            case 'totalSize':
+                return 'Tổng bộ nhớ';
+            case 'drives':
+                return 'Thông tin ổ cứng';
+            case 'displayName':
+                return 'Tên phần mềm';
+            case 'size':
+                return 'Kích thước (Bytes)';
+            case 'version':
+                return 'Phiên bản';
+            case 'installDate':
+                return 'Ngày cài đặt';
+            case 'publisher':
+                return 'Nhà cung cấp';
+            case 'installLocation':
+                return 'Đường dẫn cài đặt';
+            case 'cdROM':
+                return 'CD ROM';
             default:
                 return label;
         }
